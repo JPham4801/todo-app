cd1212a227f85c6b5fa652a3b0049c98fea05b88
JPham4801 <JPham4801@gmail.com>
2024-07-29 01:55:57 -0500

    working full CRUD with new authentication routes

--- controllers/todos.js
+++ controllers/todos.js
@@ -1,20 +1,20 @@
 const express = require('express');
-const router = express.Router()
+const router = express.Router();
 
 const User = require('../models/user.js');
 
 // Index
-router.get('/', async (req, res, next) =>{
+router.get('/', async (req, res, next) => {
   try {
     const currentUser = await User.findById(req.session.user._id);
     res.render('todos/index.ejs', {
-      todos: currentUser.todos
+      todos: currentUser.todos,
     });
   } catch (error) {
-    console.log(error)
+    console.log(error);
     res.redirect('/');
   }
-})
+});
 
 // Create
 router.get('/new', (req, res, next) => {
@@ -23,33 +23,66 @@ router.get('/new', (req, res, next) => {
 
 router.post('/', async (req, res, next) => {
   const currentUser = await User.findById(req.session.user._id);
-  currentUser.todos.push(req.body)
-  await currentUser.save()
+  currentUser.todos.push(req.body);
+  await currentUser.save();
 
   res.redirect(`/users/${currentUser._id}/todos`);
 });
 
-module.exports = router
+// Update
+router.get('/:todoId', async (req, res, next) => {
+  try {
+    const currentUser = await User.findById(req.session.user._id);
+    const todo = currentUser.todos.id(req.params.todoId);
+    res.render('todos/show.ejs', {
+      todo: todo,
+    });
+  } catch (error) {
+    console.log(error);
+    res.redirect('/');
+  }
+});
 
+router.get('/:todoId/edit', async (req, res, next) => {
+  try {
+    const currentUser = await User.findById(req.session.user._id);
+    const todo = currentUser.todos.id(req.params.todoId);
 
-// GRAVEYARD -------------------------------------------------------------------
+    res.render('todos/edit.ejs', {
+      todo: todo,
+    });
+  } catch (error) {
+    console.log(error);
+    res.redirect('/');
+  }
+});
 
-// router.get('/:todoId', async (req, res, next) => {
-  // const foundTodo = await Todo.findById(req.params.todoId);
-  // res.render('todos/show.ejs', { todo: foundTodo });
-// });
+router.put('/:todoId', async (req, res, next) => {
+  try {
+    const currentUser = await User.findById(req.session.user._id);
+    const todo = currentUser.todos.id(req.params.todoId);
 
-// router.get('/:todoId/edit', async (req, res, next) => {
-  // const foundTodo = await Todo.findById(req.params.todoId);
-  // res.render('todos/edit.ejs', { todo: foundTodo });
-// });
+    todo.set(req.body);
+    await currentUser.save();
 
-// router.put('/:todoId', async (req, res, next) => {
-  // await Todo.findByIdAndUpdate(req.params.todoId, req.body);
-  // res.redirect(`/todos/${req.params.todoId}`);
-// });
+    res.redirect(`/users/${currentUser._id}/todos/${req.params.todoId}`);
+  } catch (error) {
+    console.log(error);
+    res.redirect('/');
+  }
+});
 
-// router.delete('/:todoId', async (req, res, next) => {
-  // await Todo.findByIdAndDelete(req.params.todoId);
-  // res.redirect(`/todos`);
-// });
+router.delete('/:todoId', async (req, res, next) => {
+  try {
+    const currentUser = await User.findById(req.session.user._id);
+    currentUser.todos.id(req.params.todoId).deleteOne();
+    await currentUser.save();
+
+    res.redirect(`/users/${currentUser._id}/todos`);
+  } catch (error) {
+    console.log(error);
+    res.redirect('/');
+  }
+});
+
+module.exports = router;
